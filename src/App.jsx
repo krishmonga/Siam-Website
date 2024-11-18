7d6a3928c98d40a190e582c38346d0d1fdc5aacf
Your Name <your-email@example.com>
2024-11-18 18:54:55 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -31,22 +31,5 @@ function App() {
   )
 }
 
-export default App
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Home from './components/Home/Home'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     
-      
-    </>
-  )
-}
-
-export default App
+export default App;
+ 
